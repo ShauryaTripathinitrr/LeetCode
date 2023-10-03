51cc97ff97c87d3b708ad1dee8e2cb3d55c959fb
ShauryaTripathinitrr <146553290+ShauryaTripathinitrr@users.noreply.github.com>
2023-10-04 03:39:44 +0530

    Time: 14 ms (24.2%), Space: 6.3 MB (16.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long n=x;
-        long long k=0;
-        if(x<0)return false;
-        while(n!=0)
-        {
-            long long dig=n%10;
-            k=k*10+dig;
-            n=n/10;
-        }
-
-        return (k==x);
+        long long n=x;long long k=0;if(x<0)return false;
+ while(n!=0){long long dig=n%10;k=k*10+dig;n=n/10;}return (k==x);
     }
 };
