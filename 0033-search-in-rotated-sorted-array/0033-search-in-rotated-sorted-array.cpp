f0fa8dcfdccf5ed9196c351d658a2ad84ef90689
Shaurya Tripathi <146553290+ShauryaTripathinitrr@users.noreply.github.com>
2023-11-09 22:55:14 +0530

    Time: 2 ms (66.48%), Space: 11.2 MB (90.25%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -26,7 +26,7 @@ int binary(vector<int>& nums, int target)
         //right sorted
         else
         {
-            if(nums[mid]>=target&&nums[high]>=target)
+            if(nums[mid]<=target&&nums[high]>=target)
             {
                 low=mid+1;
             }
@@ -40,30 +40,6 @@ int binary(vector<int>& nums, int target)
 }
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
-        }
-
-        return -1;
+        return binary(nums,target);
     }
 };
