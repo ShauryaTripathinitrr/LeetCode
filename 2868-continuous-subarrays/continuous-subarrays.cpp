06d42c2af88b83decfa93ab2a0f2ba1d8e745c0d
Shaurya Tripathi <146553290+ShauryaTripathinitrr@users.noreply.github.com>
2025-05-23 17:17:34 +0530

    Time: 84 ms (84.78%) | Memory: 112.8 MB (86.85%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -3,26 +3,12 @@ class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
         ll n = nums.size();
-
-        vector<ll>diff;
-
-        for(int i = 1 ; i < n ; i++){
-            diff.push_back(nums[i] - nums[i - 1]);
-        }
-
         ll i = 0;
         ll j = 0;
-        ll k = n;
-        ll sum = 0;
         ll ans = 0;
         map<ll , ll>mp;
-        while(j < k){
+        while(j < n){
             mp[nums[j]]++;
-
-            auto f = mp.begin();
-            ll a = f -> first;
-            auto l = prev(mp.end());
-            ll b = l -> first;
 
             while(i < n && abs((prev(mp.end()) -> first) - ((mp.begin() -> first))) > 2){
                 mp[nums[i]]--;
