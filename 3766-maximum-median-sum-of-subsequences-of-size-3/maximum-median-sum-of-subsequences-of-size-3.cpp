d73dfa98cb5072c1f087096e8e4eea63310cecf1
Shaurya Tripathi <146553290+ShauryaTripathinitrr@users.noreply.github.com>
2025-07-27 10:28:58 +0530

    Time: 2760 ms (5.04%) | Memory: 174.3 MB (70.75%) - LeetSync

---
+++ 3766-maximum-median-sum-of-subsequences-of-size-3/maximum-median-sum-of-subsequences-of-size-3.cpp
@@ -0,0 +1,21 @@
+#define ll long long
+class Solution {
+public:
+    long long maximumMedianSum(vector<int>& nums) {
+        ll n = nums.size();
+
+        ll sum = 0;
+        sort(nums.begin() , nums.end());
+        int a = 0 , b = n - 2 , c = n - 1;
+
+        while(a < b){
+            cout << a << " " << b << " " << c << endl;
+            sum += nums[b];
+            c -= 2;
+            b -= 2;
+            a++;
+        }
+        return sum;
+        
+    }
+};
